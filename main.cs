2d6b3be3350cf7878dc6334c4507066c02ac3780
JaredYoung1 <143967567+JaredYoung1@users.noreply.github.com>
2023-12-12 05:08:21 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    const double x = 200;
+
+    Console.WriteLine("Enter number of hours");
+    var y = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter of Miles");
+    var z = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("With a base rate of ${0}, $150 per every hour, and $2 per every mile, the moving fee is {3} for {1} hours and {2} miles", x, y, z, (x + (y * 150) + (z * 2)));
+  }
+}
